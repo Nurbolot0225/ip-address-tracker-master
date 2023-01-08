3664a5c4f2b38f1731b72d7899324b80eede8ec2
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-01-08 14:51:34 +0600

    Dynamic work with DOM

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import {validateIp} from './helpers'
 
-const ipInput = document.querySelector('.search-bar__input')
-const btn = document.querySelector('.search-bar__btn')
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('.search-bar__btn');
 
-btn.addEventListener('click', getData)
-ipInput.addEventListener('keydown', handleKey)
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
 
 function getData() {
     if (validateIp(ipInput.value)) {
         fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_BTEx96VQechKALwxYuh6dlc3dPyQW&ipAddress=${ipInput.value}`)
         .then(response => response.json())
-        .then(console.log)    
+        .then(setInfo)
     }
 }
 
 function handleKey(e) {
     if (e.key === 'Enter') {
-        getData()
-    }
+        getData();
+    } 
+}
+
+function setInfo(mapData) {
+    console.log(mapData);
+    ipInfo.innerText = mapData.ip;
+    locationInfo.innerText = mapData.location.country + ' ' + mapData.location.region;
+    timezoneInfo.innerText = mapData.location.timezone;
+    ispInfo.innerText = mapData.isp;
 }
