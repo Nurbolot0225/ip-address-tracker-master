2fdfbe8441ad2043e8ca4e97a52a9304b5c5625a
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-01-08 14:06:51 +0600

    Processing user input

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const ipInput = document.querySelector('.search-bar__input')
+const btn = document.querySelector('.search-bar__btn')
+
+btn.addEventListener('click', getData)
+ipInput.addEventListener('keydown', handleKey)
+
+function getData() {
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_BTEx96VQechKALwxYuh6dlc3dPyQW&ipAddress=${ipInput.value}`)
+        .then(response => response.json())
+        .then(console.log)
+}
+
+function handleKey(e) {
+    if (e.key === 'Enter') {
+        getData()
+    }
+}
